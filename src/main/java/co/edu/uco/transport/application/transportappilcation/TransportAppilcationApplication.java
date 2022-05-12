0373f27c61338f8b6a65a38af21f7ed4df7aa8a3
ldjorge1988@gmail.com <ldjorge1988@gmail.com>
2022-05-11 22:30:27 -0500

    creando proyecto transportes capa de dominio

---
+++ src/main/java/co/edu/uco/transport/application/transportappilcation/TransportAppilcationApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.transport.application.transportappilcation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportAppilcationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportAppilcationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uco/transport/application/transportappilcation/TransportAppilcationApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.transport.application.transportappilcation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportAppilcationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
